5de196aca907ee435494dc55d351713fba6cf660
SergeyChudinov <sergeychudinov1986@gmail.com>
2022-07-02 13:01:05 +0500

    Make lesson-14

--- js/script.js
+++ js/script.js
@@ -226,13 +226,14 @@ window.addEventListener('scroll', showModalByScroll);
 const menu = document.querySelector('.menu__field .container');
 menu.innerHTML = '';
 class Menu {
-    constructor(img, alt, header, text, price, parentSelector) {
+    constructor(img, alt, header, text, price, parentSelector, ...classes) {
         this.img = img;
         this.alt = alt;
         this.header = header;
         this.text = text;
         this.price = price;
         this.parent = document.querySelector(parentSelector);
+        this.classes = classes;
         this.transfer = 27;
         this.chsngeToUAH();
     }
@@ -244,16 +245,20 @@ class Menu {
     // }
     getMarkup() {
         const element = document.createElement('div');
+        if (this.classes.length === 0) {
+            this.element = 'menu__item';
+            element.classList.add(this.element);
+        } else {
+            this.classes.forEach(className => element.classList.add(className))
+        }
         element.innerHTML =  `
-            <div class="menu__item">
-                <img src=${this.img} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.header}</h3>
-                <div class="menu__item-descr">${this.text}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-                </div>
+            <img src=${this.img} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.header}</h3>
+            <div class="menu__item-descr">${this.text}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
             </div>
         `;
         this.parent.append(element);
@@ -265,21 +270,27 @@ const fitnessMenu = new Menu(
 'Меню "Фитнес"', 
 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
 '229',
-'.menu__field .container');
+'.menu__field .container',
+'menu__item',
+'big');
 const premiumMenu = new Menu(
 "img/tabs/elite.jpg",
 'elite',
 'Меню “Премиум”', 
 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
 '550',
-'.menu__field .container');
+'.menu__field .container',
+'menu__item',
+'big');
 const leanMenu = new Menu(
 "img/tabs/post.jpg",
 'post',
 'Меню "Постное"', 
 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
 '430',
-'.menu__field .container');
+'.menu__field .container',
+'menu__item',
+'big');
 fitnessMenu.getMarkup();
 premiumMenu.getMarkup();
 leanMenu.getMarkup();
